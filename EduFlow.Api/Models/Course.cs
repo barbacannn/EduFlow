0fd8c1cd150fdebb3bef7dd725d89e4028646c15
barbacannn <ayushwav@gmail.com>
2025-09-08 08:12:27 -0500

    Initial commit with EduFlow project

---
+++ EduFlow.Api/Models/Course.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduFlow.Api.Models
+{
+    public class Course
+    {
+        
+    }
+}

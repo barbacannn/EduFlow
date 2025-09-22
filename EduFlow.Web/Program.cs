8eb441786a3a342082723399e7e59fa328ca65d8
barbacannn <ayushwav@gmail.com>
2025-09-22 17:57:22 -0500

    Frontend UI updates

--- EduFlow.Web/Program.cs
+++ EduFlow.Web/Program.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Net.Http;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Logging;
 using EduFlow.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// HTTP client
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-// ---- Logging for WebAssembly ----
-builder.Logging.SetMinimumLevel(LogLevel.Debug); // This ensures Debug+ logs show in the browser console
 
 await builder.Build().RunAsync();

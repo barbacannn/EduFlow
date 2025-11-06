052c729db9523dc0b6b1b4a71d851f9859405a38
barbacannn <ayushwav@gmail.com>
2025-11-06 12:02:41 -0600

    Latest frontend updates and fixes

--- EduFlow.Web/Program.cs
+++ EduFlow.Web/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using EduFlow.Web;
 using BlazorBootstrap;
+using MudBlazor.Services; 
 
 
 
@@ -9,6 +10,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazorBootstrap();
+
+builder.Services.AddMudServices();
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
